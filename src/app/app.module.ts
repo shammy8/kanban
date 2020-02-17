a5eb5f84eb84e02d08954f29478fcc2b3b97c03e
Allan Sham <allansham@hotmail.com>
2020-02-17 13:17:05 +0000

    Connect to Firebase + add AngularFire

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,14 +6,24 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { SharedModule } from './shared/shared.module';
+import { HomePageComponent } from './home-page/home-page.component';
+
+import { environment } from '../environments/environment';
+
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireAuthModule } from '@angular/fire/auth';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, HomePageComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     SharedModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule,
+    AngularFireAuthModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyB3TQyh42aqPRo7FiRNiHZD8p7cQt8eU5U',
+    authDomain: 'kanban-dbbf3.firebaseapp.com',
+    databaseURL: 'https://kanban-dbbf3.firebaseio.com',
+    projectId: 'kanban-dbbf3',
+    storageBucket: 'kanban-dbbf3.appspot.com',
+    messagingSenderId: '505778069757',
+    appId: '1:505778069757:web:7e74bd5269d7d57f541e94',
+    measurementId: 'G-JV28XDN66N',
+  },
 };
 
 /*
