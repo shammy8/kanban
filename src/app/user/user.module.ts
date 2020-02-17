28bff7368c9ac6b2cc0a3096f9c16d2e022befaf
Allan Sham <allansham@hotmail.com>
2020-02-17 15:11:36 +0000

    Add signup log in by email and password

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { UserRoutingModule } from './user-routing.module';
 import { SharedModule } from '../shared/shared.module';
 import { LoginPageComponent } from './login-page/login-page.component';
 import { GoogleSigninDirective } from './google-signin.directive';
+import { EmailLoginComponent } from './email-login/email-login.component';
 
 @NgModule({
-  declarations: [LoginPageComponent, GoogleSigninDirective],
-  imports: [CommonModule, UserRoutingModule, SharedModule],
+  declarations: [LoginPageComponent, GoogleSigninDirective, EmailLoginComponent],
+  imports: [CommonModule, UserRoutingModule, SharedModule, ReactiveFormsModule],
 })
 export class UserModule {}
