9590217e47841135d5a283c027191437d09fdd97
Allan Sham <allansham@hotmail.com>
2020-02-24 21:31:02 +0000

    Testing some firebase data modelling stuff

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,8 +1,36 @@
 import { Component } from '@angular/core';
+import {
+  AngularFirestore,
+  AngularFirestoreCollection,
+  AngularFirestoreDocument,
+} from '@angular/fire/firestore';
+import { tap, switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
   styleUrls: ['./home-page.component.scss'],
 })
-export class HomePageComponent {}
+export class HomePageComponent {
+  constructor(private db: AngularFirestore) {}
+
+  onClickMe() {
+    const password = this.db
+      .collection('posts')
+      .doc('1')
+      .valueChanges()
+      .pipe(
+        switchMap((post: { password: string }) => {
+          return this.db
+            .collection('posts with secret data')
+            .doc(post.password)
+            .valueChanges();
+        })
+      )
+      .subscribe(data => console.log(data));
+  }
+}
+
+interface Post {
+  tags: string[];
+}
